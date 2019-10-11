6ab78c1d9383cebf2a0fc3d1bda7effed5864626
CloverFeng <sunkesi02@gmail.com>
2019-10-11 15:01:29 +0800

    建立 靜態類別 ContainerConfig

---
+++ AutofacApplication/ContainerConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Interfaces;
+using Services;
+
+namespace AutofacApplication
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Congigure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<BasicCalculatorService>().As<ICalculatorService>();
+
+            return builder.Build();
+        }
+    }
+}
