00365403dcdf88260dce039ff42f0da5e6b1a82c
CloverFeng <sunkesi02@gmail.com>
2019-10-11 10:59:12 +0800

    建立類別庫專案_介面，建立計算機介面，建立計算方法

---
+++ Interfaces/ICalculatorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    /// <summary>
+    /// 計算機介面
+    /// </summary>
+    public interface ICalculatorService
+    {
+        /// <summary>
+        /// 計算方法
+        /// </summary>
+        /// <param name="inputStr">輸入的算式</param>
+        /// <returns></returns>
+        int Calculate(string inputStr);
+    }
+}
