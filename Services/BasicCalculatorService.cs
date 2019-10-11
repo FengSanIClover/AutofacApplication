a90951019b02e47fb351e1584155bd835335190d
CloverFeng <sunkesi02@gmail.com>
2019-10-11 11:14:13 +0800

    建立類別庫專案_Services，修改類別，繼承ICalculatorService 並實作方法

---
+++ Services/BasicCalculatorService.cs
@@ -0,0 +1,21 @@
+﻿using Interfaces;
+using System;
+
+namespace Services
+{
+    /// <summary>
+    /// 基礎計算機
+    /// </summary>
+    public class BasicCalculatorService : ICalculatorService
+    {
+        /// <summary>
+        /// 實作 介面的 計算方法
+        /// </summary>
+        /// <param name="inputStr">輸入的算式</param>
+        /// <returns></returns>
+        public int Calculate(string inputStr)
+        {
+            return 0;
+        }
+    }
+}
